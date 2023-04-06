05e9eaac1f9ff209e6f31c3de3ab19882610ea43
Chloe <107992972+chloeiris@users.noreply.github.com>
2023-04-06 08:59:23 +0200

    Update streamlit_app.py
    added a menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.title("My First Snowflake Data App")
+st.title("The Healthfood Diner")
+
+st.header('Breakfast Favorites')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
